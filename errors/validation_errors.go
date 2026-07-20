b7bdb191e6b75bb26db9b214a4bfaa7e0b52769a
Moynul Islam <moynul200215@gmail.com>
2026-07-20 17:44:42 +0600

    feat: implement validation logic for login request

--- errors/validation_errors.go
+++ errors/validation_errors.go
@@ -8,6 +8,8 @@ var (
 	ErrNameTooLong      = errors.New("Name must not exceed 100 characters.")
 	ErrEmailRequired    = errors.New("Email is required.")
 	ErrInvalidEmail     = errors.New("Invalid email format.")
+	ErrEmailTooLong     = errors.New("Email must not exceed 255 characters.")
 	ErrPasswordRequired = errors.New("Password is required.")
 	ErrPasswordTooShort = errors.New("Password must be at least 6 characters.")
+	ErrPasswordTooLong  = errors.New("Password must not exceed 255 characters.")
 )
