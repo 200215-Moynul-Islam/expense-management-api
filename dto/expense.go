6858822e83f0e23fc07fd959d6f3ab19733e3fb8
Moynul Islam <moynul200215@gmail.com>
2026-07-21 15:39:11 +0600

    feat: implement the service layer

--- dto/expense.go
+++ dto/expense.go
@@ -1,9 +1,23 @@
 package dto
 
+import "time"
+
 type CreateExpenseRequest struct {
 	CategoryID int     `json:"category_id" valid:"Required"`
 	Title      string  `json:"title" valid:"Required;MaxSize(255)"`
 	Amount     float64 `json:"amount" valid:"Required"`
 	Note       string  `json:"note" valid:"MaxSize(1000)"`
 	ExpenseDate string `json:"expense_date" valid:"Required"`
+}
+
+type GetExpensesRequest struct {
+	CategoryID *int
+	FromDate   *time.Time
+	ToDate     *time.Time
+
+	Page  *int
+	Limit *int
+
+	SortBy    string
+	SortOrder string
 }
