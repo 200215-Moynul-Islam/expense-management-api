109ac56dac76110cddba5ffc8bc583436995fe2c
Moynul Islam <moynul200215@gmail.com>
2026-07-21 11:37:46 +0600

    feat: implement create expense service method

--- errors/business_errors.go
+++ errors/business_errors.go
@@ -6,4 +6,8 @@ var (
 	ErrEmailExists = errors.New("Email already exists.")
 
 	ErrCategoryExists = errors.New("Category already exists.")
+
+	ErrCategoryNotFound = errors.New("Category not found.")
+	ErrForbiddenCategory = errors.New("You do not have access to this category.")
+	ErrInvalidExpenseDate = errors.New("Invalid expense date. Use YYYY-MM-DD format.")
 )
