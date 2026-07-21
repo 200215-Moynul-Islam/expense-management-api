08ad95b658a849f9bbd30c4aed6abb461f3becae
Moynul Islam <moynul200215@gmail.com>
2026-07-21 13:45:21 +0600

    feat: implement service and validator method to update a category

--- dto/category.go
+++ dto/category.go
@@ -3,3 +3,7 @@ package dto
 type CreateCategoryRequest struct {
 	Name string `json:"name" valid:"Required;MaxSize(100)"`
 }
+
+type UpdateCategoryRequest struct {
+	Name string `json:"name" valid:"Required;MaxSize(100)"`
+}
